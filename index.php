b852312539c1d872b9c2def9db5c10a94a63d71e
Lesk M <lesik.print@gmail.com>
2018-12-03 20:13:36 +0200

    lesson1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $p1 = 1+true;
+    echo "5hello ";
+    var_dump($p1);
+    ?>
+        
+</body>
+</html>
